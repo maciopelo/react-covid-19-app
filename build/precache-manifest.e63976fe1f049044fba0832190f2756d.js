db82ce0b3fea98a811ba7dfdc1a8f39c48d6330b
Maciej Koch <maciej.k00h@gmail.com>
2020-05-04 16:25:33 +0200

    gh-pages-update

---
+++ build/precache-manifest.e63976fe1f049044fba0832190f2756d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad6dc4dadb952cb21953a22637ab46e",
+    "url": "/react-covid-19-app/index.html"
+  },
+  {
+    "revision": "4f5f253af0375c308300",
+    "url": "/react-covid-19-app/static/css/main.ef4aab0b.chunk.css"
+  },
+  {
+    "revision": "39ea0eab8b0268200cb1",
+    "url": "/react-covid-19-app/static/js/2.523b6e25.chunk.js"
+  },
+  {
+    "revision": "4f5f253af0375c308300",
+    "url": "/react-covid-19-app/static/js/main.cfd8cf57.chunk.js"
+  },
+  {
+    "revision": "67f3162328234557f034",
+    "url": "/react-covid-19-app/static/js/runtime-main.d425cccf.js"
+  },
+  {
+    "revision": "e571b514ca2a42ce2a88dc9f15610b5e",
+    "url": "/react-covid-19-app/static/media/logo.e571b514.png"
+  }
+]);
